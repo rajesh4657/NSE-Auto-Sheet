b46ae9c9bceaa48275c3eb6fed84050047e6813a
rajesh4657 <rajesh4657@gmail.com>
2026-05-21 08:51:42 +0530

    Update update_sheet.py

--- update_sheet.py
+++ update_sheet.py
@@ -42,7 +42,7 @@ client = gspread.authorize(creds)
 
 # ⚠️ अपनी गूगल शीट की ID यहाँ दोबारा डालना न भूलें
 
-spreadsheet_id = "यहाँ_अपनी_शीट_की_ID_डालें" 
+spreadsheet_id = "1g3y6Jn7OH3FXRDG0RMM9ZxX7pc9zkswSW8ynCYsv2TM" 
 
 worksheet = client.open_by_key(spreadsheet_id).worksheet("Top 250 Stocks")पेनी स्टॉक
 
